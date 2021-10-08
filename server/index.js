9c6f65dd3dc8dea4ab31b72504baa5be42db2ab4
jbcostan <jbcostan@hotmail.com>
2021-10-08 17:46:42 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 
-app.unsubscribe(cors());
+app.use(cors());
 
 app.use('/posts',postRoutes);
 
