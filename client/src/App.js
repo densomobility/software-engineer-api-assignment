3c6162a5519e3e005b12c3af5b0a0a9625e61751
jbcostan <jbcostan@hotmail.com>
2021-10-08 00:11:02 -0400

    added header/image and posts/forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 
-function App() {
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import skateboarders from './images/skateboarders.jpg';
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h3" align="center">SkateBoard Market Place</Typography>
+        <img src={skateboarders} alt="skateboarders" height="160"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form /> 
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
