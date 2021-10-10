4356a04673e3ee34ac17a3df37108b4602e0624d
jbcostan <jbcostan@hotmail.com>
2021-10-10 18:12:25 -0400

    added delete in reducer

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,5 +1,7 @@
 export default (posts=[],action) =>{
     switch(action.type){
+        case 'DELETE':
+            return post.filter((post)=>post._id!=action.payload)
         case 'UPDATE':
             return posts.map((post)=> (post._id===action.payload._id?action.payload:post));
         case 'FETCH_ALL':
