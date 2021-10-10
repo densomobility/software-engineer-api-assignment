651b50fac98c16471737e714a201b8bc378c1229
jbcostan <jbcostan@hotmail.com>
2021-10-10 18:17:17 -0400

    fix syntax and call on delete dispatch in post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getPosts,createPost,updatePost} from '../controllers/posts.js'
+import {getPosts,createPost,updatePost,deletePost} from '../controllers/posts.js'
 const router = express.Router();
 
 // localhost:5000/posts
@@ -7,4 +7,5 @@ const router = express.Router();
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id',updatePost);
+router.delete('/:id',deletePost);
 export default router;
