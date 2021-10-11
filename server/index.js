c0b2e250350d62b87b317b60062a74403f6eab35
jbcostan <jbcostan@hotmail.com>
2021-10-11 00:48:49 -0400

    prep for deployment to heroku

--- server/index.js
+++ server/index.js
@@ -15,9 +15,13 @@ app.use(cors());
 
 app.use('/posts',postRoutes);
 
+app.get('/',(req,res)=>{
+    res.send('Hello to Skateboard Rest API')
+});
+
 const CONNECTION_URL ='mongodb+srv://jbcostan:P%40ssw0rd!@cluster0.fs5ci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>app.listen(PORT,()=>console.log(`Server runnning on port: http://localhost:${PORT}`)))
+.then(()=>app.listen(PORT,()=>console.log(`Server runnning on port: ${PORT}`)))
 .catch((error)=>console.log(error.message));
