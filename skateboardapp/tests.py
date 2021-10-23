8e38be657929a30278c2f3d33c328349f29e2d1f
wmati <willpmati@gmail.com>
2021-10-23 18:46:38 -0400

    Update or delete specific postings, see listed boards by user, corresponding tests for put, delete and availability links

--- skateboardapp/tests.py
+++ skateboardapp/tests.py
@@ -52,3 +52,28 @@ class get_post_put_del_test(APITestCase):
         self.assertEqual(Skateboard.objects.count(), 3)
         self.assertEqual(Skateboard.objects.get(owner="Kyle").id, 3)
         self.assertEqual(Skateboard.objects.get(owner="Kyle").owner, data['owner'])
+
+    def test_put(self):
+        url = self.home_url + 'user/Will/1/'
+        data = {"brand":"West 49"}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(Skateboard.objects.get(owner="Will").brand, data['brand'])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete(self):
+        url = self.home_url + 'user/Will/1/'
+        response = self.client.delete(url)
+        self.assertEqual(Skateboard.objects.count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_available_url(self):
+        url = self.home_url + 'available/1/'
+        response = self.client.get(url)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_unavailable_url(self):
+        url = self.home_url + 'available/0/'
+        response = self.client.get(url)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- skateboardapp/views.py
+++ skateboardapp/views.py
@@ -19,6 +19,43 @@ class SkateboardList(APIView):
 
 class SkateboardAvailable(APIView):
     def get(self, request, available, *args, **kwargs):
-        skateboard = Skateboard.objects.all().filter(availability=available)
+        skateboard = Skateboard.objects.all().filter(available=available)
         serializer = SkateboardSerializer(skateboard, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class SkateboardOwner(APIView):
+    def get(self, request, owner_name, *args, **kwargs):
+        skateboard = Skateboard.objects.all().filter(owner=owner_name)
+        serializer = SkateboardSerializer(skateboard, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class SkateboardOwnerEntry(APIView):
+    def get_object(self, owner, entry_id):
+        try:
+            return Skateboard.objects.all().filter(owner=owner, id=entry_id)
+        except Skateboard.DoesNotExist:
+            return None
+
+    def get(self, request, owner, entry_id, *args, **kwargs):
+        skateboard = self.get_object(owner, entry_id)
+        if not skateboard:
+            return Response(status.HTTP_400_BAD_REQUEST)
+        serializer = SkateboardSerializer(skateboard, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def put(self, request, owner, entry_id):
+        skateboard = self.get_object(owner, entry_id).first()
+        if not skateboard:
+            return Response(status.HTTP_400_BAD_REQUEST)
+        serializer = SkateboardSerializer(skateboard, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, owner, entry_id):
+        skateboard = self.get_object(owner, entry_id)
+        skateboard.delete()
+        return Response(
+            {"res": "Object deleted!"},
+            status=status.HTTP_200_OK)
